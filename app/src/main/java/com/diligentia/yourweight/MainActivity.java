049b7f3aaa6007015a7057dd272d4cb8726f4371
radek <radoslaw.wichrowski@gmail.com>
2016-04-08 14:23:26 +0200

    lista na pierwszym ekranie

--- app/src/main/java/com/diligentia/yourweight/MainActivity.java
+++ app/src/main/java/com/diligentia/yourweight/MainActivity.java
@@ -5,15 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import com.domain.Item;
+import com.domain.ItemsAdapter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<Item> weightList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fillTemplateWeight();
         setContentView(R.layout.activity_main);
         Button ok = (Button) findViewById(R.id.add_weight_button);
         ok.setOnClickListener(new View.OnClickListener() {
@@ -28,5 +42,43 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Construct the data source
+
+
+// Create the adapter to convert the array to views
+
+        Collections.reverse(weightList);
+        ItemsAdapter adapter = new ItemsAdapter(this, weightList);
+// Attach the adapter to a ListView
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Wybrano element " + position + ", czyli " + weightList.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+        listView.setAdapter(adapter);
+
+    }
+
+    public void fillTemplateWeight() {
+        weightList  = new ArrayList<Item>();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.DATE, -101);
+
+        BigDecimal min = new BigDecimal(80);
+        BigDecimal max = new BigDecimal(130);
+        BigDecimal randomBigDecimal;
+
+        Random r = new Random();
+        double randomValue;
+        for (int i = 0; i < 100; i++) {
+            cal.add(Calendar.DATE, 1);
+            randomBigDecimal = min.add(new BigDecimal(Math.random()).multiply(max.subtract(min)));
+            weightList.add(new Item(cal.getTime(), randomBigDecimal.setScale(2,BigDecimal.ROUND_HALF_UP)));
+        }
     }
 }
--- app/src/main/java/com/diligentia/yourweight/WeightHistoryActivity.java
+++ app/src/main/java/com/diligentia/yourweight/WeightHistoryActivity.java
@@ -21,31 +21,13 @@ import java.util.Random;
  */
 public class WeightHistoryActivity extends Activity {
 
-    ArrayList<Item> weightList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        weightList  = new ArrayList<Item>();
         setContentView(R.layout.activity_weight_history);
 
-
-        // Construct the data source
-
-        fillTemplateWeight();
-// Create the adapter to convert the array to views
-
-        ItemsAdapter adapter = new ItemsAdapter(this, weightList);
-// Attach the adapter to a ListView
-        ListView listView = (ListView) findViewById(R.id.listView);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(),
-                        "Wybrano element " + position + ", czyli " + weightList.get(position), Toast.LENGTH_SHORT).show();
-            }
-        });
-        listView.setAdapter(adapter);
 
 //        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_weight_history_item, mobileArray);
 //
@@ -71,21 +53,5 @@ public class WeightHistoryActivity extends Activity {
 //        });
     }
 
-    public void fillTemplateWeight() {
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.add(Calendar.DATE, -100);
 
-        BigDecimal min = new BigDecimal(80);
-        BigDecimal max = new BigDecimal(130);
-        BigDecimal randomBigDecimal;
-
-        Random r = new Random();
-        double randomValue;
-        for (int i = 0; i < 100; i++) {
-            cal.add(Calendar.DATE, 1);
-            randomBigDecimal = min.add(new BigDecimal(Math.random()).multiply(max.subtract(min)));
-            weightList.add(new Item(cal.getTime(), randomBigDecimal.setScale(2,BigDecimal.ROUND_HALF_UP)));
-        }
-    }
 }
