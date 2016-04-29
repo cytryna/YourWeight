82c28bf285335977e0dcd38648e411fad0a979a4
radek <radoslaw.wichrowski@gmail.com>
2016-04-29 06:07:54 +0200

    Login Activity usnięcie zbędnego odliczania ilości błędnie wprowdzanego hasła

--- app/src/main/java/com/diligentia/yourweight/LoginActivity.java
+++ app/src/main/java/com/diligentia/yourweight/LoginActivity.java
@@ -16,9 +16,6 @@ public class LoginActivity extends Activity  {
     Button b1,b2;
     EditText ed1,ed2;
 
-    TextView tx1;
-    int counter = 3;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +26,6 @@ public class LoginActivity extends Activity  {
         ed2=(EditText)findViewById(R.id.editText2);
 
         b2=(Button)findViewById(R.id.button2);
-        tx1=(TextView)findViewById(R.id.textView3);
-        tx1.setVisibility(View.GONE);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,15 +38,6 @@ public class LoginActivity extends Activity  {
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-
-                    tx1.setVisibility(View.VISIBLE);
-                    tx1.setBackgroundColor(Color.RED);
-                    counter--;
-                    tx1.setText(Integer.toString(counter));
-
-                    if (counter == 0) {
-                        b1.setEnabled(false);
-                    }
                 }
             }
         });
