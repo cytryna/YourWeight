324d98fe4dc9ed1dbad47264d2090f632948a370
radek <radoslaw.wichrowski@gmail.com>
2016-04-29 09:59:16 +0200

    Login Activity przejście po udanym zalogowaniu do MainActivity

--- app/src/main/java/com/diligentia/yourweight/LoginActivity.java
+++ app/src/main/java/com/diligentia/yourweight/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.diligentia.yourweight;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,32 +13,31 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class LoginActivity extends Activity  {
-    Button b1,b2;
-    EditText ed1,ed2;
+public class LoginActivity extends Activity {
+    Button b1, b2;
+    EditText ed1, ed2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        b1=(Button)findViewById(R.id.button);
-        ed1=(EditText)findViewById(R.id.editText);
-        ed2=(EditText)findViewById(R.id.editText2);
+        b1 = (Button) findViewById(R.id.button);
+        ed1 = (EditText) findViewById(R.id.editText);
+        ed2 = (EditText) findViewById(R.id.editText2);
 
-        b2=(Button)findViewById(R.id.button2);
+        b2 = (Button) findViewById(R.id.button2);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ed1.getText().toString().equals("admin") &&
-
-                        ed2.getText().toString().equals("admin")) {
-                    Toast.makeText(getApplicationContext(), "Redirecting...",Toast.LENGTH_SHORT).show();
-
-                }
-                else{
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
+                if (ed1.getText().toString().equals("admin") && ed2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
                 }
             }
         });
