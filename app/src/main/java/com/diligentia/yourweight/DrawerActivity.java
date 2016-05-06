4d539eece100828afd550a48c2a606c27383dba5
rwichrowski <radoslaw.wichrowski@efigence.com>
2016-05-06 07:49:35 +0200

    Ustawienia

--- app/src/main/java/com/diligentia/yourweight/DrawerActivity.java
+++ app/src/main/java/com/diligentia/yourweight/DrawerActivity.java
@@ -1,8 +1,10 @@
 package com.diligentia.yourweight;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,8 +22,16 @@ import com.diligentia.domain.ItemsAdapter;
 
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class DrawerActivity extends AppCompatActivity {
+
+    private static final String PREF_LIGHT = "pref_light";
+    private static final String PREF_WAHSING_MASHINE = "pref_washing_machine";
+    private static final String PREF_NAME = "pref_name";
+    private static final String PREF_ANIMALS = "pref_animals";
+    private static final String PREF_ICE_CREAM_FLAVOURS = "pref_ice_cream_flavours";
+    private static final String PREF_RINGTONE = "pref_ringtone";
 
     private WeightRepository weightRepository;
     private ListView mDrawerList;
@@ -49,6 +59,28 @@ public class DrawerActivity extends AppCompatActivity {
 
         Collections.reverse(weightList);
         ItemsAdapter adapter = new ItemsAdapter(this, weightList);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        boolean lightEnabled = sharedPreferences.getBoolean(PREF_LIGHT, false);
+        boolean washingMachineEnabled = sharedPreferences.getBoolean(PREF_WAHSING_MASHINE, false);
+        String nameValue = sharedPreferences.getString(PREF_NAME, "");
+        String animalValue = sharedPreferences.getString(PREF_ANIMALS, "");
+        Set<String> iceCreamFlavourValues = sharedPreferences.getStringSet(PREF_ICE_CREAM_FLAVOURS, null);
+        String ringtoneValue = sharedPreferences.getString(PREF_RINGTONE, "");
+
+        String lightValue = (lightEnabled) ? "włączone" : "wyłączone";
+        String washingMachineValue = (washingMachineEnabled) ? "włączona" : "wyłączona";
+
+
+        System.err.println(lightValue);
+//        washingMachine.setText(washingMachineValue);
+//        name.setText(nameValue);
+//        animal.setText(animalValue);
+//        iceCreamFlavour.setText(iceCreamFlavourValues.toString());
+//        ringtone.setText(ringtoneValue);
+
+
 // Attach the adapter to a ListView
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -75,7 +107,7 @@ public class DrawerActivity extends AppCompatActivity {
     }
 
     private void addDrawerItems() {
-        String[] osArray = {"Android", "iOS", "Windows", "OS X", "Linux"};
+        String[] osArray = {"Android", "iOS", "Windows", "OS X", "Ustawienia"};
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
         mDrawerList.setAdapter(mAdapter);
 
@@ -83,6 +115,10 @@ public class DrawerActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(DrawerActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+                if (position == 4) {
+                    Intent intent = new Intent(getApplicationContext(), PreferenceExampleActivity.class);
+                    startActivity(intent);
+                }
             }
         });
     }
