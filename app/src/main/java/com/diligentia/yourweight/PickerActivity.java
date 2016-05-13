28a5f44b9aed86171f063bdd6e5aef34d867a153
rwichrowski <radoslaw.wichrowski@efigence.com>
2016-05-13 07:32:45 +0200

    Zapis nowej wagi prosto z Number Picker

--- app/src/main/java/com/diligentia/yourweight/PickerActivity.java
+++ app/src/main/java/com/diligentia/yourweight/PickerActivity.java
@@ -1,11 +1,20 @@
 package com.diligentia.yourweight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.NumberPicker.OnValueChangeListener;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.diligentia.domain.Item;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 public class PickerActivity extends Activity {
 
@@ -16,6 +25,7 @@ public class PickerActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Repository repository = Repository.getInstance(this);
         setContentView(R.layout.activity_numberpicker);
 
         np1 = (NumberPicker) findViewById(R.id.numberPicker1);
@@ -44,6 +54,18 @@ public class PickerActivity extends Activity {
                 String[] split = value.split("\\.");
                 value = split[0].concat(".").concat(String.valueOf(newVal));
                 tv1.setText(value);
+            }
+        });
+
+        Button ok = (Button) findViewById(R.id.saveButton);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                repository.addWeight(new Item(new Date(), new BigDecimal(value)));
+                finish();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
     }
